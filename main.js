7149713a5cae4b99010513607608bcb8c19820cd
happyhtml1 <happyhtml1@users.noreply.github.com>
2020-03-03 22:33:35 -0300

    formulario a medias

---
+++ main.js
@@ -0,0 +1,13 @@
+function validacion(){
+    var name, lastname, mail , username, pass, cel, expresion;
+    name = document.getElementById("name").value;
+    lastname = document.getElementById("lastname").value;
+    mail = document.getElementById("correo").value;
+    username = document.getElementById("usuario").value;
+    pass = document.getElementById("pass").value;
+    cel = document.getElementById("cel").value;
+
+    expresion = /\w+@\w+\.+[a-z]/;
+
+    if (nombre === "")
+}
